12749ee0a6a3d0b1ee04012661097c0874743623
Juan Camilo Gutierrez <juan9g199@gmail.com>
2023-02-19 00:01:45 -0500

    Se elimina

--- pages/Laura.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-st.title('Hola Laura')
-st.image('https://photos.google.com/share/AF1QipPjm4EfshHPDH9yBplCH1xI9CjP5SDJmCGuxI6cQ87tpGG2HOlbVbitqRpk9zo_Rg/photo/AF1QipNx5Oy2FaEKu-SHiZJdm4xwjUI2g9PipoSCLmH6?key=R3h1cnZYemtaNjlHUnZjMlRNdHpxVEY0QWRTQ3dR')
